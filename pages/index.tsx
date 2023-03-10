805aff9aee972da2821f19153c1a51e21345bed3
can pooper <domesticatedhuman9999@gmail.com>
2023-03-10 17:14:59 -0500

    oops forgot to add some files

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,11 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '@/styles/Home.module.css'
+import styles from '@/styles/home.module.css'
 
 export default function Home() {
     return (
         <>
-            <h1 className={styles.politburo}>Communist Party of China</h1>
+            <h1 className={styles.ccpbanner}>Communist Party of China</h1>
         </>
     )
 }
