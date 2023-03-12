e620db26daa2d0b4188ff95fbe37c1c2169933f0
can pooper <domesticatedhuman9999@gmail.com>
2023-03-12 14:29:53 -0400

    burh

--- components/date.tsx
+++ components/date.tsx
@@ -3,23 +3,22 @@ import React from 'react';
 export default function Datetime() {
     const months = ["January", "February", "March", "April", "May", "June",
     "July", "August", "September", "October", "November", "December"
-];
+    ];
 
-let format = (n: string | number) => {
-    return ("00" + n).slice(-2);
-}
+    let format = (n: string | number) => {
+        return ("00" + n).slice(-2);
+    }
 
-let [year, setYear] = React.useState(1000);
-let [month, setMonth] = React.useState("January");
-let [day, setDay] = React.useState(1);
-let [hour, setHour] = React.useState(0)
-let [minute, setMinute] = React.useState(0);
-let [second, setSecond] = React.useState(0);
-let [datetime, setDatetime] = React.useState(`${day} ${month}, ${year} ${format(hour)}:${format(minute)}:${format(second)}`);
+    let [year, setYear] = React.useState(1000);
+    let [month, setMonth] = React.useState("January");
+    let [day, setDay] = React.useState(1);
+    let [hour, setHour] = React.useState(0)
+    let [minute, setMinute] = React.useState(0);
+    let [second, setSecond] = React.useState(0);
+    let [datetime, setDatetime] = React.useState(`${day} ${month}, ${year} ${format(hour)}:${format(minute)}:${format(second)}`);
 
-React.useEffect(
-    () => {
-        let update = () => {
+    React.useEffect(() => {
+        const f = setInterval(() => {
             let now = new Date();
             // year = now.getFullYear();
             // month = months[now.getMonth()]
@@ -30,21 +29,19 @@ React.useEffect(
             // 
             // let datetime = `${day} ${month}, ${year} ${format(hour)}:${format(minute)}:${format(second)}`;
             setYear(now.getFullYear());
-            setMonth(months[now.getMonth()])
+            setMonth(months[now.getMonth()]);
             setDay(now.getDate());
             setHour(now.getHours());
             setMinute(now.getMinutes());
             setSecond(now.getSeconds());
             
             setDatetime(`${day} ${month}, ${year} ${format(hour)}:${format(minute)}:${format(second)}`);
-        }
-        
-        const f = setInterval(() => {
-            update();
         }, 1000);
 
-        return () => clearInterval(f);
-    }, [])
+        return () => {
+            clearInterval(f);
+        };
+    }, []);
 
     return (
         <>
