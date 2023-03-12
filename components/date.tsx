a54b69d51fb071c3d800dad95bb9fd20cc7366c9
can pooper <domesticatedhuman9999@gmail.com>
2023-03-12 16:19:51 -0400

    fix + style

--- components/date.tsx
+++ components/date.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import styles from './date.module.css';
 
 export default function Datetime() {
     const months = ["January", "February", "March", "April", "May", "June",
@@ -15,37 +16,24 @@ export default function Datetime() {
     let [hour, setHour] = React.useState(0)
     let [minute, setMinute] = React.useState(0);
     let [second, setSecond] = React.useState(0);
-    let [datetime, setDatetime] = React.useState(`${day} ${month}, ${year} ${format(hour)}:${format(minute)}:${format(second)}`);
 
     React.useEffect(() => {
         const f = setInterval(() => {
             let now = new Date();
-            // year = now.getFullYear();
-            // month = months[now.getMonth()]
-            // day = now.getDate();
-            // hour = now.getHours();
-            // minute = now.getMinutes();
-            // second = now.getSeconds();
-            // 
-            // let datetime = `${day} ${month}, ${year} ${format(hour)}:${format(minute)}:${format(second)}`;
             setYear(now.getFullYear());
             setMonth(months[now.getMonth()]);
             setDay(now.getDate());
             setHour(now.getHours());
             setMinute(now.getMinutes());
             setSecond(now.getSeconds());
-            
-            setDatetime(`${day} ${month}, ${year} ${format(hour)}:${format(minute)}:${format(second)}`);
         }, 1000);
 
-        return () => {
-            clearInterval(f);
-        };
+        return () => clearInterval(f);
     }, []);
 
     return (
         <>
-            <p>{datetime}</p>
+            <p className={styles.inline}>{day} {month}, {year} {format(hour)}:{format(minute)}:{format(second)}</p>
         </>
     );
 }
