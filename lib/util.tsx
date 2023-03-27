50a124747c6356a8ca1cb7b79c7c249c1b7ba002
can pooper <domesticatedhuman9999@gmail.com>
2023-03-27 18:33:05 -0400

    embed test part 3842762138648172348124531276

--- lib/util.tsx
+++ lib/util.tsx
@@ -1,5 +1,15 @@
-let format = (n: string | number) => {
+export function format(n: string | number): string {
     return ("00" + n).slice(-2);
 }
 
-export { format };
+export function find(tag: string, html: string): string[] {
+    const regex = new RegExp(`<${tag}[^>]*>(.*?)<\/${tag}>`, "gi");
+    const matches = html.matchAll(regex);
+    const result: string[] = [];
+
+    for (const match of matches) {
+        result.push(match[1]);
+    }
+
+    return result;
+}
--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,12 +1,13 @@
 import { getAllPostIds, getPostData } from '../../lib/posts';
+import { find } from '../../lib/util.tsx';
 
 export default function Post({ postData }: any) {
-    let html = postData.contentHtml;
+    let html: string = postData.contentHtml;
     return (
         <>
             <header><br/></header>
-                <meta content="Communist Party of China" property="og:title"/>
-                <meta content={postData.id} property="og:description"/>
+                <meta content={find("h1", html).join(" ")} property="og:title"/>
+                <meta content={find("p", html).join("\n\n")} property="og:description"/>
                 <meta content="https://carpetmaker3162.github.io/ccp/" property="og:url"/>
                 <meta content="https://cdn.discordapp.com/emojis/1080851553080057997.webp?size=96&quality=lossless" property="og:image"/>
                 <meta content="#ff0000" data-react-helmet="true" name="theme-color"/>
