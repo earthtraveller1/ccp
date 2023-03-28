5ba99c0e381a6c86cd90b82c078b9ede1a566184
can pooper <domesticatedhuman9999@gmail.com>
2023-03-28 11:21:17 -0400

    change the css for posts

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -11,7 +11,7 @@ export default function Post({ postData }: any) {
                 <meta content="https://carpetmaker3162.github.io/ccp/" property="og:url"/>
                 <meta content="https://cdn.discordapp.com/emojis/1080851553080057997.webp?size=96&quality=lossless" property="og:image"/>
                 <meta content="#ff0000" data-react-helmet="true" name="theme-color"/>
-            <main>
+            <main class="posts">
             <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
             </main>
         </>
