8910a58ba45501f633fd7efc9766cfb5de69dc66
Liangyue " Yue <liangyuezhao7@gmail.com>
2023-04-20 19:06:32 -0400

    Initial commit

--- components/article-container.tsx
+++
@@ -1,8 +0,0 @@
-import { ReactNode } from 'react'
-import styles from './article-container.module.css'
-
-export default function ArticleContainer({ children }: { children: ReactNode }) {
-    return <div className={styles.articleContainer}>
-        {children}
-    </div>
-}
