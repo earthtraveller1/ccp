d229040219bfc3b0fd1347d9bd22ec7ddf02d3c8
Liangyue " Yue <liangyuezhao7@gmail.com>
2023-04-20 20:24:06 -0400

    Wrote the basic structure of the webpage.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,24 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import Datetime from './Datetime.svelte';
+</script>
+
+<title>Communist Party of China</title>
+
+<header>
+	<h1>Communist Party of China</h1>
+</header>
+
+<main>
+	<h1>Welcome</h1>
+	<h2>to the unofficial website of the CCP!</h2>
+
+	<p class="inline">Current date and time: </p> <Datetime />
+
+	<h2>Articles</h2>
+</main>
+
+<style>
+	.inline {
+		display: inline;
+	}
+</style>
