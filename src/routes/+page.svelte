db4cd0105f853463d4a6736a45561428a6def9df
Hello56721 <Hello56721@github.com>
2023-04-21 17:52:36 -0400

    Added the links to the articles on the home page.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,7 @@
 <script>
 	import Datetime from './Datetime.svelte';
+	import PostsContainer from './PostsContainer.svelte';
+	import Post from './Post.svelte';
 </script>
 
 <title>Communist Party of China</title>
@@ -17,6 +19,24 @@
 		<Datetime />
 
 		<h2>Articles</h2>
+
+		<PostsContainer>
+			<Post
+				title={'The Discovery of Immortality Medicine'}
+				description={'Learn how the current leader of the CCP, Neng Li, discovered immortality medicine'}
+				url={'/posts/immortality-medicine'}
+			/>
+			<Post
+				title={'CCP Anti-god weapon V4 release'}
+				description={'Learn about the newest CCP military technology, the upcoming Second Universal War, and what it means for normal people'}
+				url={'/posts/anti-god-weapon-release'}
+			/>
+			<Post
+				title={'Tensions rise between the CCP and the Univeral Forces'}
+				description={"Learn about how the CCP reacted to King Shiva Deshpande's update on his recognition of the PRC."}
+				url={'/posts/tension-ccp-universal-forces'}
+			/>
+		</PostsContainer>
 	</main>
 </div>
 
@@ -31,9 +51,9 @@
 		margin: 0;
 	}
 
-    .everything {
-        overflow: hidden;
-        max-height: 100vh;
-        margin: 0;
-    }
+	.everything {
+		overflow: hidden;
+		max-height: 100vh;
+		margin: 0;
+	}
 </style>
