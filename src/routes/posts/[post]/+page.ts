3a591e8a7b5ff4bf61b1ee5a6f1b5a8e6fe3a8e2
Hello56721 <Hello56721@github.com>
2023-04-21 17:47:59 -0400

    Formatted document.

--- src/routes/posts/[post]/+page.ts
+++ src/routes/posts/[post]/+page.ts
@@ -2,9 +2,9 @@ interface Parameters {
     post: string
 }
 
-export async function load({ params }: {params: Parameters }) {
+export async function load({ params }: { params: Parameters }) {
     const post = await import(`../${params.post}.md`)
-    const { author }: { author: string }  = post.metadata
+    const { author }: { author: string } = post.metadata
     const content = post.default
 
     return { content, author }
